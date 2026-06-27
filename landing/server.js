2b98f4270644e2c256c83740f57d9de61e0427e9
VVineey67 <jitendargoyal72@gmail.com>
2026-06-27 17:38:08 +0530

    Bol

--- landing/server.js
+++ landing/server.js
@@ -4,10 +4,17 @@ const path = require('path')
 
 const PORT = process.env.PORT || 3000
 const HTML = fs.readFileSync(path.join(__dirname, 'index.html'))
+const SITEMAP = fs.readFileSync(path.join(__dirname, 'sitemap.xml'))
 
 http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
-  res.end(HTML)
+  const url = req.url.split('?')[0]
+  if (url === '/sitemap.xml') {
+    res.writeHead(200, { 'Content-Type': 'application/xml; charset=utf-8' })
+    res.end(SITEMAP)
+  } else {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
+    res.end(HTML)
+  }
 }).listen(PORT, '0.0.0.0', () => {
   console.log(`Landing page running on port ${PORT}`)
 })
